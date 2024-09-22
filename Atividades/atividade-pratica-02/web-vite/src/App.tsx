d69251ca3964113beefa31e3a25151ab743a516a
Lê Alves <leticiaviz.alves@gmail.com>
2024-09-22 18:55:13 -0300

    WEB - Vite + React + TS
    Início da implementação da Interface Web.
    - Primeiro rascunho do menu implementado

---
+++ Atividades/atividade-pratica-02/web-vite/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+import Menu from './components/menu/Menu'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='app'>
+        <Menu/>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ Atividades/atividade-pratica-02/web-vite/src/components/menu/Menu.tsx
@@ -0,0 +1,23 @@
+const Menu = () => {
+
+    return( 
+
+
+        <div>
+            <h2>Aplicação de Doação de Sangue</h2>
+            <ul>
+                <li>Home</li>
+                <li>Estado</li>
+                <li>Cidade</li>
+                <li>Local de Coleta</li>
+                <li>Tipo sanguineo</li>
+                <li>Pessoa</li>
+            </ul>
+        </div>
+
+    );
+
+
+}
+
+export default Menu
